0f5b211d8f48b649969011a188ca5dc2ff14d88c
dferreiranjos <dferreiranjos@yahoo.com.br>
2021-07-22 21:15:55 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// TOGGLE
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for(const element of toggle){
+    element.addEventListener('click', ()=>{
+        nav.classList.toggle('show')
+    })
+}
+
+// MENU
+const links = document.querySelectorAll('nav ul li a')
+
+for(const link of links){
+    link.addEventListener('click', ()=>{
+        nav.classList.remove('show')
+    })
+}
+
+// INSERIR BOXSHADOWN NA HEADER SE A BARRA DE ROLAGEM FOR ACIONADA
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY >= navHeight){
+        header.classList.add('scroll')
+    }else{
+        header.classList.remove('scroll')
+    }
+})
