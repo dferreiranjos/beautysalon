d0df0fd9b2f1f3595408f6c81aa0a325627e6c46
dferreiranjos <dferreiranjos@yahoo.com.br>
2021-07-26 21:59:25 -0300

    testimonials and contact

--- main.js
+++ main.js
@@ -28,3 +28,29 @@ window.addEventListener('scroll', ()=>{
         header.classList.remove('scroll')
     }
 })
+
+// slider swiper
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView:1,
+    pagination:{
+        el: '.swiper-pagination'
+    },
+    mousewheel:true,
+    keyboard:true,
+});
+
+// scroll reveal: mostrar elementos quando der scroll na página
+const scrollReveal = ScrollReveal({
+    origin:'top',
+    distance:'30px',
+    duration:700,
+    reset:true
+})
+
+scrollReveal.reveal(
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links
+    `, {interval:100})
