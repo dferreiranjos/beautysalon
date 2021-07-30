236d9c7bfbd1f19bf27d0fb7f9408b03027ffb8b
dferreiranjos <dferreiranjos@yahoo.com.br>
2021-07-30 20:33:44 -0300

    responsive

--- main.js
+++ main.js
@@ -37,6 +37,12 @@ const swiper = new Swiper('.swiper-container', {
     },
     mousewheel:true,
     keyboard:true,
+    breakpoints:{
+        767:{
+            slidesPerView:2,
+            setWrapperSize:true
+        }
+    }
 });
 
 // scroll reveal: mostrar elementos quando der scroll na página
