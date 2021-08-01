ec2ed67159a5c524aa30d9939c145d1982b7d8cd
dferreiranjos <dferreiranjos@yahoo.com.br>
2021-07-31 21:00:31 -0300

    active menu and end

--- main.js
+++ main.js
@@ -18,9 +18,10 @@ for(const link of links){
 }
 
 // INSERIR BOXSHADOWN NA HEADER SE A BARRA DE ROLAGEM FOR ACIONADA
+const header = document.querySelector("#header")
+const navHeight = header.offsetHeight
+
 function changeHeaderWhenScroll(){
-    const header = document.querySelector("#header")
-    const navHeight = header.offsetHeight
 
     if(window.scrollY >= navHeight){
         header.classList.add('scroll')
@@ -64,8 +65,8 @@ scrollReveal.reveal(
 
 
 // back to top button
+const backToTopButton = document.querySelector(".back-to-top")
 function backToTop(){
-    const backToTopButton = document.querySelector(".back-to-top")
 
     if(window.scrollY >=560){
         backToTopButton.classList.add('show')
@@ -74,8 +75,34 @@ function backToTop(){
     }
 }
 
+// torna os menu selecionado ativo
+const sections = document.querySelectorAll("main section[id]")
+function activateMenuAtCurrentSection(){
+
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for( section of sections){
+        const sectionTop = section.offsetTop
+        const sectonHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectonHeight
+
+        if(checkpointStart && checkpointEnd){
+            document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('nav ul li a[href*=' + sectionId + ']').classList.remove('active')
+        }
+
+    }
+
+}
+
 // when scroll
 window.addEventListener('scroll', ()=>{
     changeHeaderWhenScroll()
     backToTop()
+    activateMenuAtCurrentSection()
 })
+
